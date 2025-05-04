12793a33651867bbd92088ad2a7153d717f51b95
Sarawut210747 <sarawut.wongsr@bumail.net>
2025-05-04 15:43:03 +0700

    1

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float jumpForce = 10f;
+    public float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    [SerializeField] private Animator animator;
+    [SerializeField] private SpriteRenderer spriteRenderer;
+    private Vector2 movement;
+    private bool isGrounded = false;
+    [SerializeField] private Transform groundCheck; // จุดเช็กพื้น
+    [SerializeField] private float groundCheckRadius = 0.2f;
+    [SerializeField] private LayerMask groundLayer;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        
+        if (movement.x != 0)
+        {
+            animator.SetBool("IsRunning", true);
+            if (movement.x > 0)
+                transform.localScale = new Vector3(1, 1, 1);
+            else
+                transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else
+        {
+            animator.SetBool("IsRunning", false); 
+        }
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+        
+        if (Input.GetButtonDown("Jump") && isGrounded)
+    {
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        animator.SetTrigger("Jump");
+    }
+    }
+     void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(movement.x * moveSpeed, rb.linearVelocity.y);
+    }
+}
